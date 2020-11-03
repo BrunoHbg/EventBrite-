1623c8f96920c5a1b5305a98d56a387ee082b95e
BrunoHBG <bruno.homberg@live.fr>
2020-11-03 20:29:51 +0100

    fourth commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,5 +67,4 @@ Rails.application.configure do
   # mailer test
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 end
